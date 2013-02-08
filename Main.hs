7314e73e2498e10baf9cb3c93026816e6446ba2f
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2013-02-08 15:00:59 +0100

    Add mainDbg to Main.hs

--- Main.hs
+++ Main.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module Main
+    (
+      main
+    , mainDbg
+    ) where
 
 import Data.List
 import System.IO
 import System.Environment
+import Control.Applicative
 
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
@@ -14,18 +19,24 @@ import qualified Data.Attoparsec.Text as A
 import Expression
 import Matcher
 
-main :: IO ()
+main :: IO (Maybe Regexp)
 main = do args <- getArgs
           prog <- getProgName
           case args of
             [pattern, file] -> do
                 input <- B.readFile file
                 case run pattern (TE.decodeUtf8 input) of
-                  Left e   -> perror ["Error:", e]
-                  Right rs -> presults rs
-            _ ->
-                perror ["Usage:", prog, "<pattern>", "<file>"]
+                  Left e         -> do perror ["Error:", e]
+                                       return Nothing
+                  Right (re, rs) -> do presults rs
+                                       return $ Just re
+            _ -> do perror ["Usage:", prog, "<pattern>", "<file>"]
+                    return Nothing
     where run p i     = do re <- A.parseOnly regexp (T.pack p)
-                           A.parseOnly (matcher re) i
+                           (,) <$> Right re <*> A.parseOnly (matcher re) i
           perror      = hPutStrLn stderr . (intercalate " ")
-          presults rs = B.putStrLn $ B.intercalate "\n--\n" $ map TE.encodeUtf8 rs
+          presults rs = B.putStr $ B.intercalate "\n--\n" $ map TE.encodeUtf8 rs
+
+mainDbg :: [String] -> IO ()
+mainDbg args = do Just re <- withArgs args main
+                  print re
